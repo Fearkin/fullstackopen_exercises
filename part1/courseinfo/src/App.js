e4d49c19d388b57a206f7b9f80d9970db95fdd57
Fearkin <aloneWorker@yandex.ru>
2023-03-23 22:07:05 +0600

    exercise 1.1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,34 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => props.content.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)
+
+const Total = (props) => <p>Number of exercises {props.total[0] + props.total[1] + props.total[2]}</p>
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content content={[
+        {id: 0, name: part1, exercises: exercises1},
+        {id: 1, name: part2, exercises: exercises2},
+        {id: 2, name: part3, exercises: exercises3},
+      ]
+      } />
+      <Total total={[exercises1, exercises2, exercises3]}/>
+    </div>
+  )
+}
+
+export default App
