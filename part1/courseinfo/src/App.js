d5354c8ead0d860e9d9d15a73d3bf2cae7a3509d
Fearkin <aloneWorker@yandex.ru>
2023-03-24 21:24:10 +0600

    exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -19,27 +19,29 @@ const Part = (props) => <p>{props.name} {props.exercises}</p>
 const Total = (props) => <p>Number of exercises {props.total[0].exercises + props.total[1].exercises + props.total[2].exercises}</p>
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content content={parts} />
-      <Total total={parts}/>
+      <Header course={course.name} />
+      <Content content={course.parts} />
+      <Total total={course.parts}/>
     </div>
   )
 }
